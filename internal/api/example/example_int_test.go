15b2247b6b12e52c705063f3940d86a1763bedea
adyusupov <adyusupov@avito.ru>
2024-03-08 15:22:13 +0300

    Tests done: tests for handler and service

--- internal/api/example/example_int_test.go
+++ internal/api/example/example_int_test.go
@@ -1,1 +1,67 @@
+//go:build integration
+
 package example_test
+
+import (
+	"github.com/stretchr/testify/suite"
+	"github.com/yusupovanton/rate-limited-api/internal/api/example"
+	"github.com/yusupovanton/rate-limited-api/internal/api/example/mocks"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+const dummyIP = "127.0.0.1"
+
+func TestHandlerSuite(t *testing.T) {
+	suite.Run(t, new(HandlerSuite))
+}
+
+type HandlerSuite struct {
+	suite.Suite
+
+	logger  *slog.Logger
+	rlMock  *mocks.RateLimiter
+	handler *example.Handler
+}
+
+func (s *HandlerSuite) SetupTest() {
+	s.rlMock = mocks.NewRateLimiter(s.T())
+	s.logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	s.handler = example.NewHandler(s.rlMock, s.logger)
+}
+
+func (s *HandlerSuite) TestHandleRequestAllowed() {
+	req, _ := http.NewRequest(http.MethodGet, "/example", nil)
+	req.RemoteAddr = dummyIP
+
+	s.rlMock.EXPECT().IsAllowed(dummyIP).Return(true).Once()
+
+	recorder := httptest.NewRecorder()
+
+	handlerFunc := s.handler.Handle()
+	handlerFunc.ServeHTTP(recorder, req)
+
+	s.Require().Equal(http.StatusOK, recorder.Code)
+}
+
+func (s *HandlerSuite) TestHandleRequestDenied() {
+	req, _ := http.NewRequest(http.MethodGet, "/example", nil)
+	req.RemoteAddr = dummyIP
+
+	s.rlMock.EXPECT().IsAllowed(dummyIP).Return(true).Times(2)
+	s.rlMock.EXPECT().IsAllowed(dummyIP).Return(false).Times(1)
+
+	recorder := httptest.NewRecorder()
+
+	handlerFunc := s.handler.Handle()
+
+	for i := 0; i < 3; i++ {
+		handlerFunc.ServeHTTP(recorder, req)
+	}
+
+	s.Require().Equal(http.StatusTooManyRequests, recorder.Code)
+}
--- internal/service/ratelimiter/ratelimiter_test.go
+++ internal/service/ratelimiter/ratelimiter_test.go
@@ -1,1 +1,93 @@
 package ratelimiter_test
+
+import (
+	"context"
+	cryptoRand "crypto/rand"
+	"fmt"
+	"github.com/stretchr/testify/suite"
+	"log/slog"
+	"math/big"
+	"os"
+	"testing"
+	"time"
+
+	rl "github.com/yusupovanton/rate-limited-api/internal/service/ratelimiter"
+)
+
+const (
+	defaultLimit     = 2
+	defaultTimeFrame = time.Second * 1
+)
+
+func TestRateLimiterService(t *testing.T) {
+	suite.Run(t, new(ServiceSuite))
+}
+
+type ServiceSuite struct {
+	suite.Suite
+
+	ctx    context.Context
+	logger *slog.Logger
+}
+
+func (s *ServiceSuite) SetupTest() {
+	s.ctx = context.Background()
+	s.logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+}
+
+func (s *ServiceSuite) TestService() {
+	service := rl.NewRateLimiter(s.logger, defaultLimit, defaultTimeFrame)
+
+	testcases := []struct {
+		name              string
+		ip                string
+		callsCount        int
+		expectedLastValue bool
+	}{
+		{
+			"single_request",
+			generateRandomIP(),
+			1,
+			true,
+		},
+		{
+			"more_than_limit",
+			generateRandomIP(),
+			3,
+			false,
+		},
+	}
+
+	for _, tc := range testcases {
+		s.Run(tc.name, func() {
+			var gotLastValue bool
+
+			for i := 0; i <= tc.callsCount; i++ {
+				gotLastValue = service.IsAllowed(tc.ip)
+			}
+
+			if tc.expectedLastValue {
+				s.Require().True(gotLastValue)
+			} else {
+				s.Require().False(gotLastValue)
+			}
+		})
+	}
+}
+
+func generateRandomIP() string {
+	return fmt.Sprintf("%d.%d.%d.%d",
+		randomInt64(256), // Generate a secure random number between 0 and 255
+		randomInt64(256),
+		randomInt64(256),
+		randomInt64(256))
+}
+
+func randomInt64(maxNum int64) int64 {
+	nBig, err := cryptoRand.Int(cryptoRand.Reader, big.NewInt(maxNum))
+	if err != nil {
+		panic(err)
+	}
+
+	return nBig.Int64()
+}
